dff3e3dd6187f804ed692fd9d85f6cae00996fb7
bshaven <bshaven7@gmail.com>
2023-08-08 17:42:04 -0400

    Added the modifiers to the copper pickaxe

--- src/main/java/com/bshaven/coppergear/item/ModItems.java
+++ src/main/java/com/bshaven/coppergear/item/ModItems.java
@@ -4,16 +4,53 @@ import com.bshaven.coppergear.CopperGear;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroups;
+import net.minecraft.item.*;
+import net.minecraft.recipe.Ingredient;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 
 public class ModItems {
 
+    private static final ToolMaterial COPPER_TOOL_MATERIAL = new ToolMaterial() {
+        @Override
+        public int getDurability() {
+            return ToolMaterials.IRON.getDurability();
+        }
+
+        @Override
+        public float getMiningSpeedMultiplier() {
+            return ToolMaterials.IRON.getMiningSpeedMultiplier();
+        }
+
+        @Override
+        public float getAttackDamage() {
+            return ToolMaterials.IRON.getAttackDamage();
+        }
+
+        @Override
+        public int getMiningLevel() {
+            return ToolMaterials.IRON.getMiningLevel();
+        }
+
+        @Override
+        public int getEnchantability() {
+            return ToolMaterials.IRON.getEnchantability();
+        }
+
+        @Override
+        public Ingredient getRepairIngredient() {
+            return ToolMaterials.IRON.getRepairIngredient();
+        }
+    };
+
     // Tools
-    public static final Item COPPER_PICKAXE = registerItem("copper_pickaxe", new Item(new FabricItemSettings()));
+    public static final Item COPPER_PICKAXE = registerItem("copper_pickaxe", new PickaxeItem(
+            COPPER_TOOL_MATERIAL, // Tool material
+            1,                    // Attack damage
+            -2.8F,                // Attack speed
+            new Item.Settings() // Item settings
+    ));
     public static final Item COPPER_AXE = registerItem("copper_axe", new Item(new FabricItemSettings()));
     public static final Item COPPER_SHOVEL = registerItem("copper_shovel", new Item(new FabricItemSettings()));
     public static final Item COPPER_SWORD = registerItem("copper_sword", new Item(new FabricItemSettings()));
